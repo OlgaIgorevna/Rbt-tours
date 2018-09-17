75a8a183aeeded3488d7e1722b394b60a8ba4116
Olga Igorevna Pron <olga.pron.kld@gmail.com>
2018-09-17 12:19:26 +0200

    главная страница

---
+++ js/script_slick_slider.js
@@ -0,0 +1,16 @@
+$(window).on("load", function () {
+
+
+
+
+	$(".slider-part").slick({
+			dots: true,
+			infinite: true,
+			speed: 300,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			slidesToShow: 1,
+			slidesToScroll: 1
+		});
+	
+});
