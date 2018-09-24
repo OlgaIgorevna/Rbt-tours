75bd69d2e007bbe79d48335be1009b7a74ce723f
Olga <olga.pron.kld@gmail.com>
2018-09-24 09:46:35 +0200

    страница о городе

--- js/script_slick_slider.js
+++ js/script_slick_slider.js
@@ -13,4 +13,49 @@ $(window).on("load", function () {
 			slidesToScroll: 1
 		});
 	
+	
+	
+	
+	$(".slider-photo").slick({
+			dots: true,
+			infinite: true,
+			speed: 300,
+			autoplay: false,
+			autoplaySpeed: 4000,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+		
+					responsive: [
+
+						{
+							breakpoint: 1200,
+							settings: {
+								slidesToShow: 3,
+								slidesToScroll: 3
+							}
+						},						
+							{
+							breakpoint: 992,
+							settings: {
+								slidesToShow: 2,
+								slidesToScroll: 2
+							}
+						},
+						{
+							breakpoint: 768,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1
+							}
+						},
+						{
+							breakpoint: 476,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1
+							}
+						}	
+						]
+		});
+	
 });
